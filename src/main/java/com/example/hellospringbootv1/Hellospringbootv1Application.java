06b0902e4902f5dd82235a5ea84aa11c246e2536
Saif <msaif.sam@gmail.com>
2021-02-08 00:34:32 +0800

    Test Spring boot files

---
+++ src/main/java/com/example/hellospringbootv1/Hellospringbootv1Application.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbootv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellospringbootv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hellospringbootv1Application.class, args);
+    }
+
+}
